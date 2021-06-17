5c902d4b9acc87e9b7d3020779146c37f7df10cb
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 12:11:53 +0800

    Update 03-pass_by_value.cpp

--- 03-pass_by_value.cpp
+++ 03-pass_by_value.cpp
@@ -2,28 +2,13 @@
 using namespace std;
 #include "swap.h"
 
-//不需要返回加void 不用寫return
-//抽取swap
-/*void swap(int num1, int num2)
-{
-	cout << "交換前: " << endl;
-	cout << "num1 = " << num1 << endl;
-	cout << "num2 = " << num2 << endl;
-	int temp = num1;
-	num1 = num2;
-	num2 = temp;
-	cout << "交換後: " << endl;
-	cout << "num1 = " << num1 << endl;
-	cout << "num2 = " << num2 << endl;
-}
-*/
+
 
 int main3() {
 
 	int a = 10;
 	int b = 20;
 	swap(a, b);
-	//值傳遞後a,b前後都不變
 
 	system("pause");
 
